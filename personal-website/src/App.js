7f53f0309e47062f8c1035716d40baca7e3fda59
John Steward <js3511@bath.ac.uk>
2025-10-06 18:57:04 +0100

    pls work

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -11,9 +11,9 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route exact path='/personal-website' component={Homepage}></Route>
-          <Route exact path='/personal-website/projects' component={ProjectsPage}></Route>
-          <Route exact path='/personal-website/hobbies' component={HobbiesPage}></Route>
+          <Route exact path='/' element={<Homepage />}></Route>
+          <Route path='/projects' element={<ProjectsPage />}></Route>
+          <Route path='/hobbies' element={<HobbiesPage />}></Route>
         </Routes>
         <Footer />
       </div>
