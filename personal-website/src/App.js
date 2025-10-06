5d9e384c174652ec2c892d15f576527d356c7c76
John Steward <js3511@bath.ac.uk>
2025-10-06 18:51:01 +0100

    another change to App

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -7,13 +7,13 @@ import Header from './Header';
 import Footer from './Footer';
 function App() {
   return (
-    <Router basename='/personal-website'>
+    <Router>
       <div className="App">
         <Header />
         <Routes>
-          <Route exact path='/' component={Homepage}></Route>
-          <Route exact path='/projects' component={ProjectsPage}></Route>
-          <Route exact path='/hobbies' component={HobbiesPage}></Route>
+          <Route exact path='/personal-website' component={Homepage}></Route>
+          <Route exact path='/personal-website/projects' component={ProjectsPage}></Route>
+          <Route exact path='/personal-website/hobbies' component={HobbiesPage}></Route>
         </Routes>
         <Footer />
       </div>
